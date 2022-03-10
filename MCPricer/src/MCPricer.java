c1cb4f576df086df9d159649870c41d8cec87e87
AliReza <alireza.khazali@gmail.com>
2022-03-10 03:47:50 +0330

    MCPricer initial implementation

---
+++ MCPricer/src/MCPricer.java
@@ -0,0 +1,60 @@
+import org.joda.time.*;
+import org.joda.time.format.*;
+
+
+
+
+class MCPricer {
+    private int numberOfSims;
+    private double [] stockPrice;
+    private double impliedVolatility;
+    private double riskFreeRate;
+    private DateTime valuationDate=new DateTime();
+    private DateTime maturityDate=new DateTime();
+    private double initialStockPrice;
+    private int duration;
+
+    /*
+    public MCPricer() {
+        numberOfSims=1000;
+    }
+    */
+
+    public MCPricer(int N, String valuationDate, double initialStockPrice, double impliedVolatility, double riskFreeRate) {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy");
+        this.numberOfSims = N;
+        this.valuationDate = formatter.parseDateTime(valuationDate);
+        this.impliedVolatility = impliedVolatility;
+        this.riskFreeRate = riskFreeRate;
+        this.initialStockPrice = initialStockPrice;
+
+        //maturityDate???
+    }
+
+    public void ConstructStockPrice() {
+        double mu = this.riskFreeRate;      //Risk-neutral assumption
+        double sigma = this.impliedVolatility;
+        int i;
+        double W = 0;
+     
+
+        duration = Days.daysBetween(valuationDate.toLocalDate(), maturityDate.toLocalDate()).getDays();
+        stockPrice = new double[duration];
+
+        for (i = 0; i < duration; i++) {
+            W += DrawFromStdNormal();
+            stockPrice[i] = initialStockPrice * Math.exp((mu - 0.5 * sigma * sigma) * (i/365.0) + sigma * W);            //A year is considered as 365 days
+        }
+
+
+        
+
+
+    }
+
+    public double DrawFromStdNormal() {
+        //change it later
+        double r=0;
+        return r;
+    }
+}
